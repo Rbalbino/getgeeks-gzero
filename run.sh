6dd9e674523b47314d78697693b3cbf31689e6de
Rafael Balbino <rafael.neco@hotmail.com>
2022-01-16 17:09:40 -0300

    adicionando report compativel com o formato junit

--- run.sh
+++
@@ -1,9 +0,0 @@
-robot -l NONE -o NONE -r NONE tasks/Delorean.robot
- #robot -d ./logs -v BROWSER:chromium -v HEADLESS:False tests
-pabot -d ./logs -v BROWSER:chromium -v HEADLESS:False tests
-
-rm -rf ./logs/browser
-mkdir ./logs/browser
-mkdir ./logs/browser/screenshot
-
-cp $(find ./logs/pabot_results -type f -name "*.png") ./logs/browser/screenshot/
