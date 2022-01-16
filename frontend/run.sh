fe6beb5bba98a0ddb5b59290bd3feeef8abfe928
Rafael Balbino <rafael.neco@hotmail.com>
2022-01-16 17:36:01 -0300

    iniciando o job de testes de regressão para web

--- frontend/run.sh
+++ frontend/run.sh
@@ -1,6 +1,6 @@
 robot -l NONE -o NONE -r NONE tasks/Delorean.robot
  #robot -d ./logs -v BROWSER:chromium -v HEADLESS:False tests
-pabot -d ./logs -v BROWSER:chromium -v HEADLESS:False tests
+pabot -x xunit.xml -d ./logs -v BROWSER:chromium -v HEADLESS:False tests
 
 rm -rf ./logs/browser
 mkdir ./logs/browser
